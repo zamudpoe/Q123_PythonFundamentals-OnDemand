bbb720908cd88e9cd4272cdcf7022af113adc416
Ing.Engelbert Zamudio Ponzzi <zamudpoe@gmail.com>
2023-01-26 19:57:44 -0600

    hello.py modificado y guardado en nuevo branch llamado sesion2

---
+++ Sesion2/hello.py
@@ -0,0 +1,10 @@
+# opcion 01 - por variable 
+nombre = "Engelbert Zamudio"
+
+# Opcion 2 - por input 
+#nombre = input('\n\t¿Cuál es tu nombre? : ')
+
+print( "\n\t" + nombre + " hello world\n") 
+
+
+
